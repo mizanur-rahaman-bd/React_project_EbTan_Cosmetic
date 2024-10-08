6e77f554c087939a07d1b956ff41aea8e262800f
mizanur-rahaman-bd <sumon1581@gmail.com>
2024-10-08 23:34:57 +0600

    Reviewe_Part_Completed

--- src/components/Common/CommonReview.jsx
+++ src/components/Common/CommonReview.jsx
@@ -4,30 +4,30 @@ import { IoIosStar } from "react-icons/io";
 const CommonReview = () => {
   return (
     <>
-    <div className='flex justify-evenly'>
+    <div className='flex justify-evenly mb-[120px]'>
       <div className='w-[441px]'>
-        <ul className='flex gap-[25px]'>
+        <ul className='flex gap-[25px] justify-center'>
             <li><IoIosStar className='text-4xl text-[#F2994A]' /></li>
             <li><IoIosStar className='text-4xl text-[#F2994A]' /></li>
             <li><IoIosStar className='text-4xl text-[#F2994A]' /></li>
             <li><IoIosStar className='text-4xl text-[#F2994A]' /></li>
             <li><IoIosStar className='text-4xl text-[#F2994A]' /></li>
         </ul>
-        <p className='font-pt_serif font-normal text-[20px] text-[#C51605] my-[13px]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
-        <h2 className='font-pt_serif font-normal text-[36px] text-[#333333]'>Ebrahim Ahmed</h2>
-        <h3 className='font-pt_serif font-normal text-[20px] text-[#F2994A]'>actor</h3>
+        <p className='font-pt_serif font-normal text-[20px] text-[#C51605] my-[13px] text-center mx-auto'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+        <h2 className='font-pt_serif font-normal text-[36px] text-[#333333] text-center mx-auto'>Ebrahim Ahmed</h2>
+        <h3 className='font-pt_serif font-normal text-[20px] text-[#F2994A] text-center mx-auto'>actor</h3>
       </div>
       <div className='w-[441px]'>
-        <ul className='flex gap-[25px]'>
+        <ul className='flex gap-[25px] justify-center'>
             <li><IoIosStar className='text-4xl text-[#F2994A]' /></li>
             <li><IoIosStar className='text-4xl text-[#F2994A]' /></li>
             <li><IoIosStar className='text-4xl text-[#F2994A]' /></li>
             <li><IoIosStar className='text-4xl text-[#F2994A]' /></li>
             <li><IoIosStar className='text-4xl text-[#F2994A]' /></li>
         </ul>
-        <p className='font-pt_serif font-normal text-[20px] text-[#C51605] my-[13px]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
-        <h2 className='font-pt_serif font-normal text-[36px] text-[#333333]'>Ebrahim Ahmed</h2>
-        <h3 className='font-pt_serif font-normal text-[20px] text-[#F2994A]'>actor</h3>
+        <p className='font-pt_serif font-normal text-[20px] text-[#C51605] my-[13px] text-center mx-auto'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+        <h2 className='font-pt_serif font-normal text-[36px] text-[#333333] text-center mx-auto'>Tanvir Shantoh</h2>
+        <h3 className='font-pt_serif font-normal text-[20px] text-[#F2994A] text-center mx-auto'>actor</h3>
       </div>
 
 
